27e13883e73453087f85494fc3839659d3d7f060
Brookzerker <brooks@brookzerker.com>
2023-02-12 10:58:18 -0700

    ran cargo fmt

--- src/pages/courses.rs
+++ src/pages/courses.rs
@@ -1,9 +1,4 @@
-use crate::{
-    api,
-    router::Routes,
-    stores::courses_store::CourseStore,
-    logging::log_error,
-};
+use crate::{api, logging::log_error, router::Routes, stores::courses_store::CourseStore};
 use ycl::{
     elements::title::BBTitleLevel,
     foundations::container::BBContainer,
