12c267e770966cf1c7fe7f2aef420543eadc7d51
Brookzerker <brooks@brookzerker.com>
2023-02-13 08:28:28 -0700

    accessibility tests passing for courses page

--- e2e_tests/accessibility.spec.ts
+++ e2e_tests/accessibility.spec.ts
@@ -2,8 +2,18 @@ import { test, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
 
 test.describe('homepage', () => {
-	test('should not have any automatically detectable accessibility issues', async ({ page }) => {
-		await page.goto('http://localhost:8082');
+	test('homepage should not have any automatically detectable accessibility issues', async ({ page }) => {
+		await page.goto('http://localhost:8082', { waitUntil: 'networkidle' });
+
+		const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+		expect(accessibilityScanResults.violations).toEqual([]);
+	});
+});
+
+test.describe('courses', () => {
+	test('courses should not have any automatically detectable accessibility issues', async ({ page }) => {
+		await page.goto('http://localhost:8082/courses');
 
 		const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
 
