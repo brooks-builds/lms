01abc4f5334868da5ca3904cce02324569c13493
Brooks Patton <BrooksPatton@users.noreply.github.com>
2023-06-04 10:19:50 -1000

    closes #15 (#16)
    * creating a courses section for live courses
    
    * showing list of live courses separately

--- src/pages/courses.rs
+++ src/pages/courses.rs
@@ -1,7 +1,9 @@
+use std::rc::Rc;
+
 use crate::{router::Routes, stores::main_store::MainStore};
 use ycl::{
     elements::title::BBTitleLevel,
-    foundations::container::BBContainer,
+    foundations::container::{BBContainer, BBContainerMargin},
     modules::{
         card_list::{BBCardData, BBCardDataBuilder, BBCardList},
         hero::BBHero,
@@ -13,17 +15,8 @@ use yewdux::prelude::use_store;
 #[function_component(Courses)]
 pub fn component() -> Html {
     let (store, _dispatch) = use_store::<MainStore>();
-    let courses = store
-        .courses
-        .iter()
-        .map(|(id, store_course)| {
-            BBCardDataBuilder::<Routes>::new()
-                .title(store_course.title.as_str())
-                .text(store_course.short_description.as_str())
-                .link(Routes::CourseDetails { id: *id })
-                .build()
-        })
-        .collect::<Vec<BBCardData<Routes>>>();
+    let courses = create_card_data_list(store.clone(), false);
+    let live_courses = create_card_data_list(store, true);
 
     html! {
         <BBContainer>
@@ -31,12 +24,40 @@ pub fn component() -> Html {
                 text="All the courses available"
                 title="Course Library"
             />
+            <BBContainer margin={BBContainerMargin::Normal}>
+            <BBCardList<Routes>
+                card_data={live_courses}
+                card_title_level={BBTitleLevel::Three}
+                title_level={BBTitleLevel::Two}
+                title="Live Courses"
+            />
             <BBCardList<Routes>
                 card_data={courses}
                 card_title_level={BBTitleLevel::Three}
                 title_level={BBTitleLevel::Two}
                 title="Featured Courses"
             />
+            </BBContainer>
         </BBContainer>
     }
+}
+
+fn create_card_data_list(store: Rc<MainStore>, live: bool) -> Vec<BBCardData<Routes>> {
+    store
+        .courses
+        .iter()
+        .filter_map(move |(id, store_course)| {
+            if store_course.live != live {
+                return None;
+            }
+
+            Some(
+                BBCardDataBuilder::<Routes>::new()
+                    .title(store_course.title.as_str())
+                    .text(store_course.short_description.as_str())
+                    .link(Routes::CourseDetails { id: *id })
+                    .build(),
+            )
+        })
+        .collect::<Vec<BBCardData<Routes>>>()
 }
