2402dd73d2f572986af783ceb9f580b2c71b384d
Brookzerker <brooks@brookzerker.com>
2023-07-04 08:55:10 -0600

    set up test stub for creating a course

---
+++ e2e_tests/create_course.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Visitor", async () => {
+	test("cannot see the create course link", async ({ page }) => {
+		throw new Error("todo");
+	});
+});
