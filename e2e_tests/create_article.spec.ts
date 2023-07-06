5b34004455639bef22040247b4844a20996e81b5
Brookzerker <brooks@brookzerker.com>
2023-07-06 07:40:43 -0600

    testing that learners cannot create articles

--- e2e_tests/create_article.spec.ts
+++ e2e_tests/create_article.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { interceptGraphql } from "./graphql_intercepter";
+import { Role, login } from "./utils";
 
 test.beforeEach(async ({ page }) => { await interceptGraphql(page) });
 
@@ -14,6 +15,27 @@ test.describe("Visitor", async () => {
 
   test("cannot navigate directly to create article page", async ({ page }) => {
     await page.goto("/create_article");
+    await page.waitForURL("/");
+    await page.waitForTimeout(150);
+
+    const errorMessage = page.getByText("Authors can create");
+    const isVisible = await errorMessage.isVisible({ timeout: 60000 });
+
+    expect(isVisible).toBe(true);
+  });
+});
+
+test.describe("Learners", async () => {
+  test("cannot see the create article link", async ({ page }) => {
+    await login(Role.Learner, page, "/create_article");
+    await page.waitForURL("/");
+    await page.waitForTimeout(50);
+
+    expect(await page.getByRole("link", { name: "Create Article" }).isVisible()).toBe(false);
+  });
+
+  test("cannot navigate directly to create article page", async ({ page }) => {
+    await login(Role.Learner, page, "/create_article");
     await page.waitForURL("/");
     await page.waitForTimeout(150);
 
