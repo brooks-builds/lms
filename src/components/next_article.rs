4c70fcb0caeb68710623f9058b81afa3eb94cbaa
Brookzerker <brooks@brookzerker.com>
2023-09-21 19:56:41 -0600

    fixed formatting issues

--- src/components/next_article.rs
+++ src/components/next_article.rs
@@ -29,11 +29,11 @@ pub fn component(props: &Props) -> Html {
     let onclick = Callback::from(move |_| {
         props_onclick.emit(article_id);
     });
-    let Some(next_article) = next_article(course, props.article_id, store.own_course(props.course_id)) else { 
+    let Some(next_article) = next_article(course, props.article_id, store.own_course(props.course_id)) else {
         let logged_in = store.logged_in();
         return html! {
             <BBButton onclick={onclick} button_style={BBButtonStyle::PrimaryLight} disabled={!logged_in}>{"Complete Article"}</BBButton>
-        } 
+        }
     };
     let title = format!("Complete and goto next article: {}", &next_article.title);
 
