db478ddd2334e4b50572778198c63224b0346a9f
Brookzerker <brooks@brookzerker.com>
2024-03-25 11:02:01 -0600

    added cards for about us

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,8 +1,12 @@
 use ycl::{
-    elements::{icon::BBIconType, title::BBTitleLevel},
+    elements::{
+        icon::BBIconType,
+        text::BBText,
+        title::{BBTitle, BBTitleLevel},
+    },
     foundations::container::{BBContainer, BBContainerMargin},
     modules::{
-        card_list::{BBCardData, BBCardDataBuilder, BBCardList},
+        card_list::{BBCardData, BBCardDataBuilder, BBCardDataWidth, BBCardList},
         hero::BBHero,
     },
 };
@@ -14,9 +18,34 @@ use crate::router::Routes;
 pub fn component() -> Html {
     html! {
         <BBContainer margin={BBContainerMargin::Normal}>
-            <BBHero text="Welcome to Brooks Builds Learning. Here you can find high-quality education-first courses on Rust"
+            <BBHero text="Welcome to Brooks Builds Learning. Here you can dive into high-quality, education-first programming courses covering a range of topics including Rust, Docker, and more. The courses are designed to build your expertise from the ground up."
                 title="Brooks Builds Learning"
             />
+            <BBTitle level={BBTitleLevel::Two}>
+                {"What is Brooks Builds?"}
+            </BBTitle>
+            <BBText>
+                {"Back when Brooks was teaching coding at a CodeSchool he had a segment called 'courage time'. This was a week where students were challenged to create something in an unfamiliar language and/or framework. The most common complaint was 'I can't build something in a language until I've learned it'. Brooks then began his Twitch stream, modeling how to learn while doing and called it Brooks Builds."}
+            </BBText>
+            <BBText>
+                {"While Brooks isn't teaching at that CodeSchool anymore, he is still teaching on Twitch and Youtube where you can watch him learn, create projects, as well as thes courses available here."}
+            </BBText>
+            <BBTitle level={BBTitleLevel::Two}>
+                {"What makes us different?"}
+            </BBTitle>
+            <BBText>
+                {"This LMS (Learning Management System) is focused primarily on Brooks' style of teaching. We are concerned primarily with your learning and therefore won't be 'selling' you on other courses or content while taking a course."}
+            </BBText>
+            <BBText>
+                {"Since Brooks created this LMS he has complete control over it and is safe from companies prioritizing selling over teaching."}
+            </BBText>
+            <BBCardList<Routes>
+                card_data={about_us_cards_data()}
+                card_title_level={BBTitleLevel::Two}
+                icon={BBIconType::Star}
+                title="About Us"
+            />
+
             <BBCardList<Routes>
                 card_data={community_cards_data()}
                 card_title_level={BBTitleLevel::Two}
@@ -34,6 +63,20 @@ fn community_cards_data() -> Vec<BBCardData<Routes>> {
             .text("Join our community on Discord to get help with courses, programming, and stay up to date with the latest BB news")
             .href("https://discord.gg/y7GkU6UMrm")
             .href_text("Join Discord")
+            .width(BBCardDataWidth::Small)
             .build(),
+    ]
+}
+
+fn about_us_cards_data() -> Vec<BBCardData<Routes>> {
+    vec![
+        BBCardDataBuilder::new()
+            .title("What is Brooks Builds?")
+            .text("Back when Brooks was teaching coding at a CodeSchool he had a segment called 'courage time'. This was a week where students were challenged to create something in an unfamiliar language and/or framework. The most common complaint was 'I can't build something in a language until I've learned it'. Brooks then began his Twitch stream, modeling how to learn while doing and called it Brooks Builds. While Brooks isn't teaching at that CodeSchool anymore, he is still teaching on Twitch and Youtube where you can watch him learn, create projects, as well as thes courses available here.")
+            .build(),
+        BBCardDataBuilder::new()
+            .title("What makes us different?")
+            .text("This LMS (Learning Management System) is focused primarily on Brooks' style of teaching. We are concerned primarily with your learning and therefore won't be 'selling' you on other courses or content while taking a course. Since Brooks created this LMS he has complete control over it and is safe from companies prioritizing selling over teaching.")
+            .build()
     ]
 }
